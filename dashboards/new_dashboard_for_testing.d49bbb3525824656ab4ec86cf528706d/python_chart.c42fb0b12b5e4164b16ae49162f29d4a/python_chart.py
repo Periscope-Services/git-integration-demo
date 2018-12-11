7bd6f0369d7db52c03f1a7299f4cda422ab096aa
Periscope Data <periscope@periscopedata.com>
2018-12-11 16:39:08 +0000

    Robert Friedland created Chart: python chart

---
+++ dashboards/new_dashboard_for_testing.d49bbb3525824656ab4ec86cf528706d/python_chart.c42fb0b12b5e4164b16ae49162f29d4a/python_chart.py
@@ -0,0 +1,4 @@
+# SQL output is imported as a dataframe variable called 'df'
+# Use Periscope to visualize a dataframe or show text by passing data to periscope.table() or periscope.text() respectively. Show an image by calling periscope.image() after your plot.
+
+periscope.output(df)
