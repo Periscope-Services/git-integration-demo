8e03d6239057eb6c6b8e0aeb55196da10ff9c42e
Periscope Data <periscope@periscopedata.com>
2018-12-27 20:39:03 +0000

    Ryan Waters updated Chart: Gas Heatmap

--- dashboards/utilities.d49f3e10151e4ffc8033a170fb420ba6/gas_heatmap.c48e9bec796c450f90467236744ec361/gas_heatmap.py
+++ dashboards/utilities.d49f3e10151e4ffc8033a170fb420ba6/gas_heatmap.c48e9bec796c450f90467236744ec361/gas_heatmap.py
@@ -1,11 +1,12 @@
 # SQL output is imported as a dataframe variable called 'df'
 # Use Periscope to visualize a dataframe or show text by passing data to periscope.table() or periscope.text() respectively. Show an image by calling periscope.image() after your plot.
-
+import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
 
 df = df.pivot("month", "day", "usage")
-print df
-ax = sns.heatmap(df, cmap="YlGnBu", square=True, cbar=False)
-periscope.output(ax)
+grid_kws = {"height_ratios": (.9, .05), "hspace": .3}
+f, (ax, cbar_ax) = plt.subplots(2,gridspec_kw=grid_kws)
+fig = sns.heatmap(df, ax=ax, cbar_ax=cbar_ax, cbar_kws={"orientation": "horizontal"}, cmap="YlGnBu", square=True)
+periscope.output(fig)
