af6410fc25552a2699bd34174f0a79791b039bd2
Periscope Data <periscope@periscopedata.com>
2018-12-27 20:07:49 +0000

    Ryan Waters created Chart: Gas Heatmap

---
+++ dashboards/utilities.d49f3e10151e4ffc8033a170fb420ba6/gas_heatmap.c48e9bec796c450f90467236744ec361/gas_heatmap.py
@@ -0,0 +1,12 @@
+# SQL output is imported as a dataframe variable called 'df'
+# Use Periscope to visualize a dataframe or show text by passing data to periscope.table() or periscope.text() respectively. Show an image by calling periscope.image() after your plot.
+
+import numpy as np
+import seaborn as sns
+
+
+# print df
+df = df.pivot("month", "day", "usage")
+print df
+ax = sns.heatmap(df)
+periscope.output(ax)
---
+++ dashboards/utilities.d49f3e10151e4ffc8033a170fb420ba6/gas_heatmap.c48e9bec796c450f90467236744ec361/gas_heatmap.sql
@@ -0,0 +1,9 @@
+select
+  [date:month_of_year] as month
+  , [date:day_of_month] as day
+  , usage
+
+from
+  [gas_data]
+where
+  [date=daterange]
