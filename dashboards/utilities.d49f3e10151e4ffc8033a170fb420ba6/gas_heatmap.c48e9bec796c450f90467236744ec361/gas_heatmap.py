c65256e921c6831348bfb7d8839abe2abd483f5a
Periscope Data <periscope@periscopedata.com>
2018-12-27 21:26:27 +0000

    Ryan Waters updated Chart: Gas Heatmap

--- dashboards/utilities.d49f3e10151e4ffc8033a170fb420ba6/gas_heatmap.c48e9bec796c450f90467236744ec361/gas_heatmap.py
+++ dashboards/utilities.d49f3e10151e4ffc8033a170fb420ba6/gas_heatmap.c48e9bec796c450f90467236744ec361/gas_heatmap.py
@@ -6,7 +6,10 @@ import seaborn as sns
 
 
 df = df.pivot("month", "day", "usage")
-grid_kws = {"height_ratios": (.9, .05), "hspace": .3}
+grid_kws = {"height_ratios": (.9, .05), "hspace": .5}
 f, (ax, cbar_ax) = plt.subplots(2,gridspec_kw=grid_kws)
-fig = sns.heatmap(df, ax=ax, cbar_ax=cbar_ax, cbar_kws={"orientation": "horizontal"}, cmap="YlGnBu", square=True)
-periscope.output(fig)
+
+f = sns.heatmap(df, ax=ax, cbar_ax=cbar_ax, cbar_kws={"orientation": "horizontal"}, cmap="YlGnBu")
+f.set_yticklabels(f.get_yticklabels(), rotation=0)
+f.set_xticklabels(f.get_xticklabels(), rotation=0)
+periscope.output(f)
