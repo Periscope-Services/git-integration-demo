85f7b6728d74fcfa18f5094a5d1cfb363b61256e
Periscope Data <periscope@periscopedata.com>
2018-12-27 23:07:13 +0000

    Ryan Waters updated Chart: Gas Usage Heatmap

--- dashboards/utilities.d49f3e10151e4ffc8033a170fb420ba6/gas_usage_heatmap.c48e9bec796c450f90467236744ec361/gas_usage_heatmap.py
+++ dashboards/utilities.d49f3e10151e4ffc8033a170fb420ba6/gas_usage_heatmap.c48e9bec796c450f90467236744ec361/gas_usage_heatmap.py
@@ -1,5 +1,6 @@
 # SQL output is imported as a dataframe variable called 'df'
 # Use Periscope to visualize a dataframe or show text by passing data to periscope.table() or periscope.text() respectively. Show an image by calling periscope.image() after your plot.
+
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
@@ -7,12 +8,17 @@ import seaborn as sns
 days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
 df = df.pivot("month", "day", "usage")
+
+# Rename columns to string names
 df.columns = days
 
 grid_kws = {"height_ratios": (.9, .03), "hspace": .4}
 f, (ax, cbar_ax) = plt.subplots(2,gridspec_kw=grid_kws, figsize=(12,7))
 
 f = sns.heatmap(df, ax=ax, cbar_ax=cbar_ax, cbar_kws={"orientation": "horizontal", "label": "Therms"}, cmap="gist_heat")
+
+# Rotate the axis labels
 f.set_yticklabels(f.get_yticklabels(), rotation=0)
 f.set_xticklabels(f.get_xticklabels(), rotation=0)
+
 periscope.output(f)
