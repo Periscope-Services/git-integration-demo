1eed8888f6be7742245bac50832f233885dffaa3
Periscope Data <periscope@periscopedata.com>
2019-01-08 18:43:17 +0000

    Tristan Nunnally updated View: tristan_demo_view

---
+++ views/tristan_demo_view/tristan_demo_view.py
@@ -0,0 +1,4 @@
+# SQL output is imported as a dataframe variable called 'df'
+# Use Periscope to materialize a dataframe by passing the dataframe to periscope.materialize()
+
+periscope.materialize(df)
