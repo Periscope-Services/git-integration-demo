5d5b865178e105f922e36363bed61c7c28135c05
Periscope Data <periscope@periscopedata.com>
2019-03-13 17:31:18 +0000

    Post-create commit for chart Revenue | [aggregation] by Periscope
    External id: c477d5b3885f442088ee519755b426ae

--- dashboards/git_demo.d470ff20f66645daa10bb26de1fc21ee/revenue_clone_chart_1/revenue____aggregation_.py
+++
@@ -1,5 +0,0 @@
-# SQL output is imported as a dataframe variable called 'df'
-# Use Periscope to visualize a dataframe or show text by passing data to periscope.table() or periscope.text() respectively. Show an image by calling periscope.image() after your plot.
-
-print 'Hello, World'
-periscope.output(df)
