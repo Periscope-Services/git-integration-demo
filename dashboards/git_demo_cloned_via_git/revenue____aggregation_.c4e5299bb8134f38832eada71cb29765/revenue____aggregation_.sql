35c1e0950cce00a24d01ac12ada30cab16427b91
asubbarao <aloksubbarao@gmail.com>
2019-03-13 10:57:18 -0700

    adding cloned dashboard

---
+++ dashboards/git_demo_cloned_via_git/revenue____aggregation_.c4e5299bb8134f38832eada71cb29765/revenue____aggregation_.sql
@@ -0,0 +1,14 @@
+select
+  [date(purchase_date):aggregation]
+  , purchase_platform
+  , sum(price)
+--hi
+from
+  [demo_data_view]
+where
+--   [platform=device]
+--   and [source=origin]
+  [purchase_date=daterange]
+group by
+  1
+  , 2
