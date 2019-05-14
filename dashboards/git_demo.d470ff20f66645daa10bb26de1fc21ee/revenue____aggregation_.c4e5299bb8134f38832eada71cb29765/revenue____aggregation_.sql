b86b99ad4f475b03f3e95222029fade08310c2c8
dempsey-kyle <kyledemp@gmail.com>
2019-05-14 06:47:45 -0700

    Update revenue____aggregation_.sql

--- dashboards/git_demo.d470ff20f66645daa10bb26de1fc21ee/revenue____aggregation_.c4e5299bb8134f38832eada71cb29765/revenue____aggregation_.sql
+++ dashboards/git_demo.d470ff20f66645daa10bb26de1fc21ee/revenue____aggregation_.c4e5299bb8134f38832eada71cb29765/revenue____aggregation_.sql
@@ -2,14 +2,9 @@ select
   [date(purchase_date):aggregation]
   , purchase_platform
   , sum(price)
---hi
-
---test1111
 from
   [demo_data_view]
 where
---   [platform=device]
---   and [source=origin]
   [purchase_date=daterange]
 group by
   1
