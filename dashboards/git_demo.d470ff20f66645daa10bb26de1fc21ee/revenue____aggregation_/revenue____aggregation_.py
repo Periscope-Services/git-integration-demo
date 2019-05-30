fc273660ee63a11d2353c112ebdf8d881b661bd2
Kyle Dempsey <kyledempsey@Kyles-MacBook-Pro.local>
2019-05-29 22:46:21 -0700

    testing

--- dashboards/git_demo.d470ff20f66645daa10bb26de1fc21ee/revenue____aggregation_/revenue____aggregation_.py
+++ dashboards/git_demo.d470ff20f66645daa10bb26de1fc21ee/revenue____aggregation_/revenue____aggregation_.py
@@ -1,5 +1,5 @@
 # SQL output is imported as a dataframe variable called 'df'
 # Use Periscope to visualize a dataframe or show text by passing data to periscope.table() or periscope.text() respectively. Show an image by calling periscope.image() after your plot.
 
-print 'Hello, World'
+print 'Hello, World 123'
 periscope.output(df)
